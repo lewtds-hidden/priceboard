36708d6fcaaf81828f49eaec5ad4057a0bcab68d
trung.vuong <trung.vuong@vndirect.com.vn>
2015-10-30 16:24:43 +0700

    format price & quantity in the same manner as in the current priceboard

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,19 @@
 module.exports = {
     formatPrice(price) {
-        return "" + (price / 1000);
+        if (price === 0) {
+            return '';
+        }
+        return (price / 1000).toFixed(1);
     },
 
     formatQty(qty) {
-        return "" + (qty / 100);
+        if (qty === 0) {
+            return '';
+        }
+        // quantities from priceservice have already been divided by 10
+        // this regex wont work for decimal numbers, but that's ok
+        var formatted = (qty * 10).toString().replace(/(\d)(?=(?:[0-9]{3})+\b)/, '$1,');
+        return formatted.substr(0, formatted.length - 1); // trim the last digit
+    },
     }
 }

452d7ee009b205da99a76b4d3a0cf9b8acb22c88
trung.vuong <trung.vuong@vndirect.com.vn>
2015-11-02 11:43:23 +0700

    fix the regex pattern for price formatting (it missed the global flag, lol)

--- scripts/utils.js
+++ scripts/utils.js
@@ -12,7 +12,7 @@ module.exports = {
         }
         // quantities from priceservice have already been divided by 10
         // this regex wont work for decimal numbers, but that's ok
-        var formatted = (qty * 10).toString().replace(/(\d)(?=(?:[0-9]{3})+\b)/, '$1,');
+        var formatted = (qty * 10).toString().replace(/(\d)(?=(?:[0-9]{3})+\b)/g, '$1,');
         return formatted.substr(0, formatted.length - 1); // trim the last digit
     },
 

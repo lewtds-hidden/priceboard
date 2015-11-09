ef1c03a43d3ad4c15682d2a7fb8243b5e6a6fba8
Trung Ngo <ndtrung4419@gmail.com>
2015-11-09 22:41:34 +0700

    Change deploy script to clone the local repo

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 rm -rf build
-mkdir build && cd build
-git init
-git remote add origin https://github.com/chin-hidden/priceboard
-git fetch && git checkout gh-pages
+
+git clone . build
+cd build
+git checkout gh-pages
 
 cd .. && make
 
+cd build
 git add *
 git commit -m "Deploying $(cd ..; git describe --long --tags --dirty --alway)"
 git push origin gh-pages
